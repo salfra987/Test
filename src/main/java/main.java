fa58028dec0c61dcb5cd518c19de958a03174d27
salfra987 <salfra@live.unc.edu>
2022-11-05 16:12:58 -0400

    Push 3

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.Buffer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +13,14 @@ public class main {
         new URL(
             "http://api.weatherapi.com/v1/current.json?key=67ebbdcece194ee0942174811220511&q=Paris&aqi=no");
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    System.out.println(con.getResponseMessage());
+    for (int i = 0; i < 10; i++) {
+      System.out.println(con.getHeaderField(i));
+    }
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    String line = null;
+    while ((line = in.readLine()) != null) {
+      System.out.println(line);
+    }
   }
 }
