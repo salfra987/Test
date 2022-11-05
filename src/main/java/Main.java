e4d49aad21034ba3bafeccbed9291a05b0655134
salfra987 <salfra@live.unc.edu>
2022-11-05 15:15:30 -0400

    Push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,14 @@ import java.net.URL;
 public class Main {
 
   public static void Main() throws IOException {
-   /* URL url = new URL("http://api.weatherapi.com/v1");
+    /* URL url = new URL("http://api.weatherapi.com/v1");
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("GET");*/
 
-      URL url = new URL("http://api.weatherapi.com/v1/current.json?key=67ebbdcece194ee0942174811220511&q=London&aqi=no");
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
+    URL url =
+        new URL(
+            "http://api.weatherapi.com/v1/current.json?key=67ebbdcece194ee0942174811220511&q=London&aqi=no");
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
         }
 }
